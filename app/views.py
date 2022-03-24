833814303965acd5e10a76bc27a52d0e322dab8a
a.polukhin <a.polukhin@carbonsoft.ru>
2022-03-25 00:19:10 +0500

    formatting to syslog

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ import aiohttp_jinja2
 import aiohttp.web as web
 import service
 from config import SRV
-import json
+from config import logger
 
 
 @aiohttp_jinja2.template("mainpage.jinja2")
@@ -11,25 +11,31 @@ async def mainpage(request):
 
 
 async def start_button(request):
+    logger.info(f'{request.remote} is starting {SRV} service.')
     return web.json_response(await service.start())
 
 
 async def stop_button(request):
+    logger.info(f'{request.remote} is stopping {SRV} service.')
     return web.json_response(await service.stop())
 
 
 async def restart_button(request):
+    logger.info(f'{request.remote} is restarting {SRV} service.')
     return web.json_response(await service.restart())
 
 
 async def status_button(request):
+    logger.info(f'{request.remote} requested {SRV} service status.')
     return web.json_response(await service.status())
 
 
 async def checkbox_status(request):
+    logger.info(f'{request.remote} requested checkbox status.')
     return web.json_response({'checkbox': await service.checkbox()})
 
 
 async def checkbox_save(request):
     await service.checkbox_save(request.rel_url.query['checkbox'])
+    logger.info(f'{request.remote} set checkbox to {request.rel_url.query["checkbox"]}.')
     return web.json_response('ok')
--- config.py
+++ config.py
@@ -1,1 +1,12 @@
+import logging
+import logging.handlers
+
+logger = logging.getLogger('logger')
+formatter = logging.Formatter(fmt='service-control[%(process)d]: %(levelname)s: %(message)s')
+handler = logging.handlers.SysLogHandler(address='/dev/log')
+handler.formatter = formatter
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
+
+
 SRV = 'chronyd'
