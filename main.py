d29a5086993eb06ea9ff5ec6ef262b796cb6d062
a.polukhin <a.polukhin@carbonsoft.ru>
2022-03-25 00:22:10 +0500

    if name main

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import jinja2
 import aiohttp_jinja2
 from app.routes import setup
 
-app = web.Application()
-aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader("app/templates"))
-setup(app)
-web.run_app(app, port=80)
+
+if __name__ == '__main__':
+    app = web.Application()
+    aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader("app/templates"))
+    setup(app)
+    web.run_app(app, port=80)
