cb8c6af03a0d86cf52e2444bd1d7ffc006e2bf82
a.polukhin <a.polukhin@carbonsoft.ru>
2022-03-24 16:05:55 +0500

    button functionality

--- app/routes.py
+++ app/routes.py
@@ -3,3 +3,6 @@ from app import views
 
 def setup(app):
     app.router.add_get('/', views.mainpage)
+    app.router.add_get('/start', views.start_button)
+    app.router.add_get('/stop', views.stop_button)
+    app.router.add_get('/restart', views.restart_button)
--- app/views.py
+++ app/views.py
@@ -1,8 +1,22 @@
 import aiohttp_jinja2
+from service import *
 
 
 @aiohttp_jinja2.template("mainpage.jinja2")
 async def mainpage(request):
     print('wtf')
-    return {'active': None,
-            'error': 'Something is wrong'}
+    return await status()
+
+@aiohttp_jinja2.template("mainpage.jinja2")
+async def start_button(request):
+    return await start()
+
+
+@aiohttp_jinja2.template("mainpage.jinja2")
+async def stop_button(request):
+    return await stop()
+
+
+@aiohttp_jinja2.template("mainpage.jinja2")
+async def restart_button(request):
+    return await restart()
